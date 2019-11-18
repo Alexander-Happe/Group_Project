8245c4178df7b5d0012da9cc2fdf4d1b76ab6ffe
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-18 17:57:21 -0600

    adds new list item and close class. close doesnt work yet

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.ajax({
     url:
-      "https://api.edamam.com/search?q=chicken&app_id=60ab0f71&app_key=c79295660b6df898dbeb376b6fbd7821",
+      "https://api.edamam.com/search?q={+}&app_id=60ab0f71&app_key=c79295660b6df898dbeb376b6fbd7821",
     method: "GET"
   }).then(function(response) {
     console.log(response);
@@ -12,6 +12,7 @@ $(document).ready(function() {
     $(".here").append(newDiv);
   });
   // adds user input from the shopping list input to the shopping list//
+
   $("#addItemBtn").on("click", function() {
     var newItem = $("#addItem")
       .val()
@@ -21,17 +22,20 @@ $(document).ready(function() {
       .text(newItem);
     $("#shoppingListItems").prepend(newListItem);
     $("#addItem").val("");
+    // adds a close button to the list items. //
+    var addSpan = $("<span>")
+      .attr("class", "close")
+      .text("X");
+    $("li").append(addSpan);
   });
 
-  // adds a close button to the list items. //
-  var myShoppingList = $(".listItems");
-  console.log($("#shoppingListItems"));
-  var i;
-  for (i = 0; i < myShoppingList.length; i++) {
-    var span = $("SPAN").attr("class", "close");
-    console.log(span);
-    var txt = document.createTextNode("X");
-    span.appendChild(txt);
-    myShoppingList[i].appendChild(span);
-  }
+  // Click on a close button to hide the current list item
+  // var close = $(".close");
+  // var i;
+  // for (i = 0; i < close.length; i++) {
+  //   close[i].onclick = function() {
+  //     var div = this.parentElement;
+  //     div.style.display = "none";
+  //   };
+  // }
 });
