1a3d5cbc2584baa01bb96be71f11f665ec7bfd57
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-19 12:44:28 -0600

    strick through works, delete does not yet

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,7 @@ $(document).ready(function() {
   });
   // adds user input from the shopping list input to the shopping list//
 
-  $("#addItemBtn").on("click", function(event) {
-    event.prevent.default;
+  $("#addItemBtn").on("click", function() {
     // takes in item from add textarea
     var newItem = $("#addItem")
       .val()
@@ -35,25 +34,20 @@ $(document).ready(function() {
     // resets the texts field to blank
     $("#addItem").val("");
   });
-  // Add a "checked" symbol when clicking on a list item
-  var list = $("#");
-  list.addEventListener(
-    "click",
-    function(ev) {
-      if (ev.target.tagName === "LI") {
-        ev.target.classList.toggle("checked");
-      }
-    },
-    false
-  );
 
-  // Click on a close button to hide the current list item
-  var close = $(".close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
+  // Add a "checked" symbol when clicking on a list item
+  $(document).on("click", ".listItems", checked);
+
+  function checked() {
+    var myItem = $(this).toggleClass("checked");
   }
+  // Click on a close button to hide the current list item
+  // var close = $(".close");
+  // var i;
+  // for (i = 0; i < close.length; i++) {
+  //   close[i].onclick = function() {
+  //     var div = this.parentElement;
+  //     div.style.display = "none";
+  //   };
+  // }
 });
