9c66a4f2ffd9172ccd31b040b921ae94b90f3436
Grayson Mechler <Graysonmechler@gmail.com>
2019-11-18 19:43:44 -0600

    adding a new pull

--- assets/script.js
+++ assets/script.js
@@ -13,29 +13,47 @@ $(document).ready(function() {
   });
   // adds user input from the shopping list input to the shopping list//
 
-  $("#addItemBtn").on("click", function() {
+  $("#addItemBtn").on("click", function(event) {
+    event.prevent.default;
+    // takes in item from add textarea
     var newItem = $("#addItem")
       .val()
       .trim();
+    // creates div for item and button
+    var newDiv = $("<div>");
+    // creates li //
     var newListItem = $("<li>")
       .attr("class", "listItems")
       .text(newItem);
-    $("#shoppingListItems").prepend(newListItem);
-    $("#addItem").val("");
     // adds a close button to the list items. //
     var addSpan = $("<span>")
       .attr("class", "close")
       .text("X");
-    $("li").append(addSpan);
+    // adds item and close span to the new div //
+    newDiv.append(newListItem, addSpan);
+    $("#shoppingList").prepend(newDiv);
+    // resets the texts field to blank
+    $("#addItem").val("");
   });
+  // Add a "checked" symbol when clicking on a list item
+  var list = $("#");
+  list.addEventListener(
+    "click",
+    function(ev) {
+      if (ev.target.tagName === "LI") {
+        ev.target.classList.toggle("checked");
+      }
+    },
+    false
+  );
 
   // Click on a close button to hide the current list item
-  // var close = $(".close");
-  // var i;
-  // for (i = 0; i < close.length; i++) {
-  //   close[i].onclick = function() {
-  //     var div = this.parentElement;
-  //     div.style.display = "none";
-  //   };
-  // }
+  var close = $(".close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 });
