690a93ae07bb8f178552a7e42db0c856756cefbb
Devin <alexanderdev117@gmail.com>
2019-11-20 20:30:20 -0600

    css search fixes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
     $(".main-search-button").on("click", function(){
         var userInput = $(".main-search").val().trim()
         $.ajax({
@@ -52,66 +51,6 @@ $(document).ready(function() {
     })
     
 
-=======
-  $(".main-search-button").on("click", function() {
-    var userInput = $(".main-search")
-      .val()
-      .trim();
-    $.ajax({
-      url:
-        "https://api.edamam.com/search?q=" +
-        userInput +
-        "&app_id=aa7e15d0&app_key=95bc405156202f2376c3c63ef483565b",
-      method: "GET"
-    }).then(function(response) {
-      var hits = response.hits;
-      console.log(response.hits);
-      $(".dish-results").empty();
-      for (i = 0; i < hits.length; i++) {
-        //loops through and makes a new div for each recipe
-        var recipeDiv = $("<div>");
-        recipeDiv.addClass("name column is-5 has-text-centered subtitle");
-        //adds name of the recipes
-        var names = hits[i].recipe.label;
-        recipeDiv.text(names);
-        //adds div for buttons (style) ****This is so bulma understands how to lay out the buttons
-        var resultbuttons = $("<div>");
-        resultbuttons.addClass("columns is-mobile is-centered");
-        recipeDiv.append(resultbuttons);
-        //adds links to the divs
-        var links = $("<button>");
-        var recipeLinks = hits[i].recipe.url;
-        links.addClass(
-          "column is-5-desktop is-5-tablet is-5-mobile search-item"
-        );
-        links.attr("href", recipeLinks);
-        links.text("Recepie Source");
-        resultbuttons.append(links);
-        //adds buttons for adding ingredients
-        var ingredientBttn = $("<button>");
-        ingredientBttn.text("See Ingredients");
-        ingredientBttn.attr(
-          "class",
-          "ingredients-button column is-5-desktop is-5-tablet is-5-mobile search-item"
-        );
-        ingredientBttn.attr("value", hits[i].recipe.ingredientLines);
-        resultbuttons.append(ingredientBttn);
-        //makes new image tags
-        var imgs = $("<img>");
-        var sourceImg = hits[i].recipe.image;
-        imgs.attr("src", sourceImg);
-        imgs.attr("alt", "recipe img");
-        imgs.attr("class", "img image is-250x250");
-        //adds image to the divs
-        recipeDiv.append(imgs);
-        $(".dish-results").append(recipeDiv);
-      }
-    });
-  });
-  $(document).on("click", ".ingredients-button", function() {
-    console.log($(this).val());
-    console.log("working");
->>>>>>> master
   });
   // adds user input from the shopping list input to the shopping list//
 
